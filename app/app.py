0c95711f8823762422e8cec3a1ce083640aec405
yn-coder <yn.coder@gmail.com>
2026-07-26 13:35:16 +0300

    #5 Update app.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 from litestar import Litestar
 
 from .config import template_config
-from .user_portal.views import User_Portal_Controller
-from .star_fortress.views import Star_Fortress_Controller
+from .user_portal.up_views import User_Portal_Controller
+from .star_fortress.sf_views import Star_Fortress_Controller
 
 app = Litestar( route_handlers=[User_Portal_Controller, Star_Fortress_Controller],
     template_config=template_config,
