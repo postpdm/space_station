891194f7d5267af68e91e7a7550aa26c44ed2d96
yn-coder <yn.coder@gmail.com>
2026-07-28 11:41:17 +0300

    #11 #18 requeirement for pydantic-settings and env file settings

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from litestar import Litestar
+#from litestar.config.allowed_hosts import AllowedHostsConfig
+from litestar.di import Provide
 
-from .config import template_config, static_config
+from .config import template_config, static_config, get_settings
 from .user_portal.up_views import User_Portal_Controller
 from .star_fortress.sf_views import Star_Fortress_Controller
 from .core.core_config import db_plugin
@@ -8,9 +10,23 @@ from .core.core_view import NewsController
 
 from .plugins import get_all_ss_plugins
 
+settings = get_settings()
+
+# Security: Limit domains to prevent HTTP Host Header attacks
+# host_config = AllowedHostsConfig(
+#    allowed_hosts=settings.allowed_hosts,
+#    exclude=["/health"] # Allow load balancer checks
+#)
+
 plugins_list = get_all_ss_plugins()
 
-app = Litestar( route_handlers=[User_Portal_Controller, Star_Fortress_Controller, NewsController],
+app = Litestar( debug=settings.litestar_debug, # Hard disable debug mode in prod!
+                # allowed_hosts=host_config,
+                
+                # Inject settings globally via dependency injection
+                dependencies={"app_settings": Provide(get_settings, use_cache=True, sync_to_thread=False )},
+                
+                route_handlers=[User_Portal_Controller, Star_Fortress_Controller, NewsController],
                 template_config=template_config,
                 static_files_config=[static_config],
                 plugins=[db_plugin] + plugins_list,
--- app/config.py
+++ app/config.py
@@ -3,6 +3,10 @@ from litestar.plugins.jinja import JinjaTemplateEngine
 from litestar.template.config import TemplateConfig
 from litestar.static_files import StaticFilesConfig
 
+from functools import lru_cache
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 # Define the path to your templates directory
 TEMPLATE_DIR = Path(__file__).parent / "templates"
@@ -23,3 +27,30 @@ static_config = StaticFilesConfig(
     directories=[STATIC_DIR], # List of directory paths to look up files
     html_mode=False,         # Set to True if serving a Single Page App (SPA) index.html
 )
+
+
+class AppSettings(BaseSettings):
+    # Litestar specific native flags
+    litestar_debug: bool = Field(default=True, validation_alias="LITESTAR_DEBUG")
+    
+    # App specific variables
+    environment: str = "dev"
+    #secret_key: str
+    #allowed_hosts: list[str]
+    #database_url: str
+    
+    # Auth section
+    AM_I_USER_URL : str
+    AM_I_USER_FIELD : str
+
+    
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+@lru_cache()
+def get_settings() -> AppSettings:
+    """Returns a cached settings instance to prevent repeated parsing."""
+    return AppSettings()
