77b576963af87dd5c275cbc93e3de6a03cab6bf1
yn-coder <yn.coder@gmail.com>
2026-08-16 10:46:28 +0300

    #55 cms shema

--- app/plugins/CMS/controller.py
+++ app/plugins/CMS/controller.py
@@ -1,14 +1,34 @@
+from uuid import UUID
+
+from typing import Annotated
+
 from litestar import Controller, get
 from litestar.response import Template
+from litestar.params import Dependency, PathParameter
+
+from advanced_alchemy.extensions.litestar import (
+    filters,
+    providers,
+    service,
+)
 
 from app.plugins.abc_controller import BasePluginController
 
 CMS_TEMPLATES_DIR = "CMS/"
 
 from .service import CMSService
+from .schema import Page_pdnt
 
 class CMS_Controller(BasePluginController):
     path = "/CMS"
+
+    dependencies = providers.create_service_dependencies(
+        CMSService,
+        "CMS_service",
+        #load=[GNN_Article_Model.sections],
+        filters={"pagination_type": "limit_offset", "id_filter": UUID, "search": "title", "search_ignore_case": True},
+    )
+
 
     @get("/")
     async def user_homepage(self) -> Template:
@@ -23,3 +43,15 @@ class CMS_Controller(BasePluginController):
 
     async def plugin_health(self) -> bool:
         return True
+
+    @get(path="/get_pages")
+    async def list_pages(
+        self,
+        CMS_service: CMSService,
+        filters: Annotated[list[filters.FilterTypes], Dependency(skip_validation=True)],
+    ) -> service.OffsetPagination[Page_pdnt]:
+        """List pages."""
+        results, total = await CMS_service.get_many_and_count(*filters)
+        return CMS_service.to_schema(results, total, filters=filters, schema_type=Page_pdnt)
+
+#
---
+++ app/plugins/CMS/schema.py
@@ -0,0 +1,23 @@
+import datetime
+from uuid import UUID
+from typing import Annotated, Optional
+
+from pydantic import BaseModel
+
+class Page_pdnt(BaseModel):
+    id: Optional[UUID] = None
+    title: str
+    content : str
+    published_dt: Optional[datetime.datetime] = None
+    created_user_id : UUID
+    #created_user : User_pdnt
+
+#class NewsCreate_pdnt(BaseModel):
+#    title: str
+#    content : str
+#    published_dt: Optional[datetime.datetime] = None
+
+#class NewsUpdate_pdnt(BaseModel):
+#    title: str
+#    content : str
+#    published_dt: Optional[datetime.datetime] = None
