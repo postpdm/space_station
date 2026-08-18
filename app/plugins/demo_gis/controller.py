a0ab2b08f2652608ab1f1e62d3bdb59a71e2c3bb
yn-coder <yn.coder@gmail.com>
2026-08-18 16:57:38 +0300

    #13 #56 GIS -> gis for unix ways

--- app/plugins/demo_gis/controller.py
+++ app/plugins/demo_gis/controller.py
@@ -3,16 +3,22 @@ from litestar.response import Template
 
 from app.plugins.abc_controller import BasePluginController
 
-DG_TEMPLATES_DIR = "demo_GIS/"
+DG_TEMPLATES_DIR = "demo_gis/"
 
 class Demo_GIS_Controller(BasePluginController):
-    path = "/demo_GIS"
+    path = "/demo_gis"
 
     @get("/")
     async def user_homepage(self) -> Template:
+        geoserver_cfg = { 'proj' : 'EPSG:3857',
+                          'size' : 256,
+
+
+                           };
+
         return Template(
-            template_name = DG_TEMPLATES_DIR + "demo_GIS.html",
-            context={  }
+            template_name = DG_TEMPLATES_DIR + "demo_gis.html",
+            context={ 'geoserver_cfg' :geoserver_cfg }
         )
 
     @get("/admin_panel")
