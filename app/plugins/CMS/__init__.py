e55af3543cc6694ddd1b6e12e0adb4d2f9a40480
yn-coder <yn.coder@gmail.com>
2026-08-20 13:50:13 +0300

    #58 mermaid for preview

--- app/plugins/CMS/__init__.py
+++ app/plugins/CMS/__init__.py
@@ -14,6 +14,9 @@ class CMS_Plugin(BasePlugin):
     fuser_description = 'Content management'
     fplugin_id = UUID( '6def0698-aa92-45f7-b0b7-946b59845dce' )
 
-    fstatic_req = [ 'pico/2.1.1/pico.classless.min.css', 'pico/2.1.1/minimal-theme-switcher.js', 'pico/2.1.1/modals.js' ]
+    fstatic_req = [ 'pico/2.1.1/pico.classless.min.css', 'pico/2.1.1/minimal-theme-switcher.js', 'pico/2.1.1/modals.js',
+                    'markdown/marked/18.0.9/marked.umd.js', 
+                    'markdown/mermaid/11.17.0/mermaid.min.js',
+    ]
 
 #
