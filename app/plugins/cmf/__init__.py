3263c9311e699baaa390f00f400efe854615965c
yn-coder <yn.coder@gmail.com>
2026-08-23 20:45:27 +0300

    #56 try to temporaly rename CMS->cmf

---
+++ app/plugins/cmf/__init__.py
@@ -0,0 +1,22 @@
+from uuid import UUID
+
+from litestar.config.app import AppConfig
+
+from .controller import CMS_Controller
+# take abstract
+from app.plugins.abc_plugin import BasePlugin
+
+class CMS_Plugin(BasePlugin):
+    # add routing
+    controllers = [CMS_Controller]
+
+    fuser_title = 'CMS'
+    fuser_description = 'Content management'
+    fplugin_id = UUID( '6def0698-aa92-45f7-b0b7-946b59845dce' )
+
+    fstatic_req = [ 'pico/2.1.1/pico.classless.min.css', 'pico/2.1.1/minimal-theme-switcher.js', 'pico/2.1.1/modals.js',
+                    'markdown/marked/18.0.9/marked.umd.js', 
+                    'markdown/mermaid/11.17.0/mermaid.min.js',
+    ]
+
+#
