61c6595190285efb84ba81a5f01d09f60b07ab2a
yn-coder <yn.coder@gmail.com>
2026-08-24 17:01:48 +0300

    #63 view bar statistic with Chartist

--- app/plugins/orion_cms/__init__.py
+++ app/plugins/orion_cms/__init__.py
@@ -17,6 +17,9 @@ class CMS_Plugin(BasePlugin):
     fstatic_req = [ 'pico/2.1.1/pico.classless.min.css', 'pico/2.1.1/minimal-theme-switcher.js', 'pico/2.1.1/modals.js',
                     'markdown/marked/18.0.9/marked.umd.js', 
                     'markdown/mermaid/11.17.0/mermaid.min.js',
+                    ### https://github.com/chartist-js/chartist
+                    'charts/chartist/0.11.4/chartist.min.css',
+                    'charts/chartist/0.11.4/chartist.min.js'
     ]
 
 #
