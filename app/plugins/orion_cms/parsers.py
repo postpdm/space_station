c0b8f4bd1229f1820c7fbc05a579cd07896d4e73
yn-coder <yn.coder@gmail.com>
2026-08-27 09:53:05 +0300

    #76 try wrapper

--- app/plugins/orion_cms/parsers.py
+++ app/plugins/orion_cms/parsers.py
@@ -22,30 +22,36 @@ async def execute_orion_manusctript( code : str, db_session: AsyncSession ) -> s
     res = ''
     for line in code.splitlines():
         if line.lower() == 'show table':
-            res = '<table border="2">'
-            if not executed:
-                query = await db_session.execute( prepared_select )
-                dataset = query.all()
-                res += '<thead><tr>'
+            try:
+                res = '<table border="2">'
+                if not executed:
+                    query = await db_session.execute( prepared_select )
+                    dataset = query.all()
+                    res += '<thead><tr>'
 
-                # table headers
-                headers = query.keys()
-                for header in headers:
-                  res += '<th>' + header + '</th>'
+                    # table headers
+                    headers = query.keys()
+                    for header in headers:
+                      res += '<th>' + header + '</th>'
 
-                executed = True
-            res += '</tr></thead><tbody>'
-            for row in dataset:
-                res += '<tr>'
-                row_dict = row._mapping
-                for value in row:   
-                    res += '<td>' + str( value ) + '</td>'
-                res += '</tr>'
+                    executed = True
+                res += '</tr></thead><tbody>'
+                for row in dataset:
+                    res += '<tr>'
+                    row_dict = row._mapping
+                    for value in row:   
+                        res += '<td>' + str( value ) + '</td>'
+                    res += '</tr>'
 
-            res += '</tbody></table>'
+                res += '</tbody></table>'
+            except Exception as e:
+                res = 'Error in SQL execution ' + e
         else:
             if (line.lower() ).startswith('select'):
-                prepared_select = await build_select( line )
+                try:
+                    prepared_select = await build_select( line )
+                except Exception as e:
+                    res = 'Error in SQL parsing ' + e
             else:
                 res = 'Unknown command ' + line
 
