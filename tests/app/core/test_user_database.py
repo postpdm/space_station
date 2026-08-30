8b7142a78cec48ac2a4c85990f8e9f580cba5f06
yn-coder <yn.coder@gmail.com>
2026-08-30 13:19:41 +0300

    #81 test user model

---
+++ tests/app/core/test_user_database.py
@@ -0,0 +1,26 @@
+import pytest
+from litestar.testing import AsyncTestClient
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+#from datetime import date
+
+from app.core.core_models import User
+
+@pytest.mark.asyncio
+async def test_create_user(db_session: AsyncSession):
+    """Test direct interaction with the database session."""
+    # Arrange
+    new_user = User( user_login="TEST_USER", user_name="Mr. Test User" )
+    db_session.add(new_user)
+    await db_session.flush()  # Push to DB within the active transaction
+    
+    # Act
+    result = await db_session.execute(select(User).where(User.user_login=="TEST_USER"))
+    user = result.scalar_one_or_none()
+
+    # Assert
+    assert user is not None
+    assert user.user_login == "TEST_USER"
+    assert user.user_name == "Mr. Test User"
+
