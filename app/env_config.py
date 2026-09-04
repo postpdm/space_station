1abb39d8fd55daefe407ebc186688e43b2c8c49d
yn-coder <yn.coder@gmail.com>
2026-09-04 10:41:04 +0300

    #83 #85 default values for env settings

--- app/env_config.py
+++ app/env_config.py
@@ -25,21 +25,21 @@ class AppSettings(BaseSettings):
         return value
         
     # Auth section
-    AM_I_USER_URL : str
-    AM_I_USER_LOGIN_FIELD : str
-    AM_I_USER_NAME_FIELD : str
+    AM_I_USER_URL : str | None = None
+    AM_I_USER_LOGIN_FIELD : str | None = None
+    AM_I_USER_NAME_FIELD : str | None = None
     AM_I_USER_SERVER_REQUEST : bool = True
     
     
     # GIS GeoServer settings
     GIS_USE_GEOSERVER : bool = False
-    PROJECTION : str
-    TILE_SIZE : int
-    WMTS_ATTRIBUTIONS : str
-    WMTS_URL : HttpUrl
-    WMTS_LAYER : str
-    WMS_URL : HttpUrl
-    WMS_LAYERS : list[str]
+    PROJECTION : str | None = None
+    TILE_SIZE : int | None = None
+    WMTS_ATTRIBUTIONS : str | None = None
+    WMTS_URL : HttpUrl | None = None
+    WMTS_LAYER : str | None = None
+    WMS_URL : HttpUrl | None = None
+    WMS_LAYERS : list[str] | None = None
     
     model_config = SettingsConfigDict(
         env_file=".env", 
