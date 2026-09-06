5152b49651ddfc699e406bb0fceb8267db5b1389
yn-coder <yn.coder@gmail.com>
2026-09-06 03:47:56 +0300

    #39 inner_circle still required

--- tests/app/test_config.py
+++ tests/app/test_config.py
@@ -1,10 +1,14 @@
 import pytest
 
+import secrets
+
 from app.config import AppSettings
 
 @pytest.mark.asyncio
 async def test_CDN_false() -> None:
-    fake_env_data = { "USE_CDN" : False }
+    hex_key = secrets.token_hex(16)
+
+    fake_env_data = { "inner_circle_key" : hex_key, "USE_CDN" : False }
 
     # Init config
     test_config = AppSettings(**fake_env_data)
@@ -14,8 +18,9 @@ async def test_CDN_false() -> None:
 
 @pytest.mark.asyncio
 async def test_CDN_true() -> None:
-
-    fake_env_data = { "USE_CDN" : True }
+    hex_key = secrets.token_hex(16)
+    
+    fake_env_data = { "inner_circle_key" : hex_key, "USE_CDN" : True }
 
     # Init config
     test_config = AppSettings(**fake_env_data)
