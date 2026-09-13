f3e1a14a6a282878479d1e519e1b0d8c70304bd3
yn-coder <yn.coder@gmail.com>
2026-09-13 20:41:19 +0300

    #95 pytest for external db connection

---
+++ tests/app/core/test_core_ext_db_service.py
@@ -0,0 +1,33 @@
+import pytest
+
+import asyncio
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+#from sqlalchemy.exc import IntegrityError
+
+from app.core.core_ext_db_service import check_ext_db_connection
+
+pytestmark = pytest.mark.asyncio
+
+#@pytest.mark.asyncio
+async def test_ext_db_connection_success():
+    """Test connect to right external db, for SQLite memory sample."""
+    # Arrange
+
+    valid_url = "sqlite+aiosqlite:///:memory:"
+
+    success, message = await check_ext_db_connection(valid_url)
+
+    assert success is True
+    assert message == "✅ Database connection established successfully!"
+
+async def test_ext_db_connection_wrong_port_error():
+    """Test connect to wrong external db."""
+    invalid_url = "UUUUUUU://user:pass@127.0.0.1:9999/wrong_db"
+
+    success, message = await check_ext_db_connection(invalid_url)
+
+    assert success is False
+    assert message == "❌ Connection error: Can't load plugin: sqlalchemy.dialects:UUUUUUU"
