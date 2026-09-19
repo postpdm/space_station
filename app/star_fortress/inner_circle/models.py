965f862a93f20eb9b8d7575647afc7654531a30c
yn-coder <yn.coder@gmail.com>
2026-09-19 18:34:31 +0300

    #113 validate `expire_dt`, if it empty or contain a garbage (sqlite is not type check for default, may be somebody put wrong dates to our db? )

--- app/star_fortress/inner_circle/models.py
+++ app/star_fortress/inner_circle/models.py
@@ -2,10 +2,12 @@ from datetime import datetime, timezone
 from typing import Optional
 from advanced_alchemy.base import UUIDAuditBase
 from advanced_alchemy.types import EncryptedString
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, validates
+
+from space_station_stc.relic_transmission.validate_dt import SafeDateTime
 
 from .context import db_encryption_key
-
+        
 class ExternalDB(UUIDAuditBase):
     __tablename__ = "external_db"
 
@@ -18,9 +20,20 @@ class ExternalDB(UUIDAuditBase):
     connection_pw: Mapped[Optional[str]] = mapped_column(EncryptedString( default=None, key = lambda: db_encryption_key.get().get_secret_value() ) )
     
     # expire datetime
-    expires_at: Mapped[Optional[datetime]] = mapped_column(index=True)
-
+    expires_at: Mapped[Optional[datetime]] = mapped_column(SafeDateTime, index=True, nullable=True )
+    
+    @validates('expires_at')
+    def validate_expires_at(self, key, value):
+        # Triggered when WRITING or UPDATING via SQLAlchemy
+        # empry field is verbotten, SET NULL instead        
+        if isinstance(value, str) and value.strip() == '':
+            return None
+        return value
+        
     # check expires
     @property
     def is_expired(self) -> bool:
-        return datetime.now(timezone.utc) >= self.expires_at
+        if self.expires_at:
+            return datetime.now(timezone.utc) >= self.expires_at
+        else:
+            return False
